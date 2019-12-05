88eab1856e63b67f8b720d3d66c17c82b4ab4240
YunHsin Kuo <yhkuo@campus-022-222.ucdavis.edu>
2019-12-05 15:29:02 -0800

    update .gitignore and make sure moveImg.py works for everyone

--- moveImg.py
+++ moveImg.py
@@ -2,9 +2,10 @@ import os
 import shutil
 
 # This script copy all images from rootdir to targetdir and match the structure BubbleNets needs
-
-rootdir = '/Users/rogerlee/Desktop/ECS269/DAVIS/JPEGImages/480p/'
-targetdir = '/Users/rogerlee/Desktop/ECS269/Project/BubbleNets/data/rawData/'
+# Make sure DAVIS dataset is under the folder BubbleNets.
+mainDir = os.getcwd()
+DAVISDir = os.path.join(mainDir, 'DAVIS', 'JPEGImages', '480p')
+targetdir = os.path.join(mainDir, 'data', 'rawData')
 
 ###############################################################
 # WARNING: WILL REMOVE EVERYTHING FROM TARGET DIRECTORY FIRST #
@@ -12,14 +13,20 @@ targetdir = '/Users/rogerlee/Desktop/ECS269/Project/BubbleNets/data/rawData/'
 os.system("rm -rf " + targetdir + "*")
 
 
-subdir = os.listdir(rootdir)
+subdir = os.listdir(DAVISDir)
 for dir in subdir:
     if(dir == '.DS_Store'):
         continue
-    os.system("mkdir " + targetdir + dir + "/")
-    os.system("mkdir " + targetdir + dir + "/src")
+    addDir = os.path.join(targetdir, dir ,'src')
+    if not os.path.exists(addDir):
+        os.makedirs(addDir)
 
-for root, dirs, files in os.walk(rootdir):
-        for file in files:
-            if file != '.DS_Store':
-                shutil.copyfile(os.path.join(root, file), targetdir + str(root).split('/')[-1] + "/src/" + file)
+for root, dirs, files in os.walk(DAVISDir):
+    path_list = root.split(os.sep)
+    videoName = path_list[-1]
+    for file in files:
+        if videoName == "480p":
+            continue;
+        else:
+            imageDir = os.path.join(targetdir, videoName, 'src')
+            shutil.copy(os.path.join(DAVISDir, videoName, file), imageDir)
